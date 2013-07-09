5beda72064f3cfa9cac370115be3ba459ac5db3c
Mathaeus Dejori <dejori@gmail.com>
2013-07-08 21:28:02 -0700

    changed script

--- python-hadoop/run-zipcode-script.sh
+++ python-hadoop/run-zipcode-script.sh
@@ -1,6 +1,6 @@
 # copy http://www2.census.gov/econ2007/CBP_CSV/zbp07detail.zip 
 # unpack data
-# copy file on hdfs hadoop fs -put /zbp07detail.txt /data-folder/
+# copy file onto hdfs: hadoop dfs -copyFromLocal /local-data-folder /data-folder/
 
 hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-streaming-xxx.jar \
 -input /data-folder/zbp07detail.txt \
