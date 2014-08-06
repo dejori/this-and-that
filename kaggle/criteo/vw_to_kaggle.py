e0567d470c29aa04468de618213c397598956bf7
mathaeus dejori <dejori@gmail.com>
2014-08-06 15:37:20 -0700

    gzip submission file

--- kaggle/criteo/vw_to_kaggle.py
+++ kaggle/criteo/vw_to_kaggle.py
@@ -1,7 +1,7 @@
 __author__ = 'mdejori'
 
 import math
-
+import gzip
 
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
@@ -12,3 +12,9 @@ with open("data/submission.csv", "wb") as outfile:
     for line in open("data/preds.txt"):
         row = line.strip().split(" ")
         outfile.write("%s,%f\n" % (row[1], sigmoid(float(row[0]))))
+
+# and create a gzip version for faster upload
+with open("data/submission.csv", "rb") as infile:
+    f_out = gzip.open('data/submission.csv.gz', 'wb')
+    f_out.writelines(infile)
+    f_out.close()
